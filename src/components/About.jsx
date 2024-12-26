250ba1a4fc3465fb4f4e67fef0c34c0811ac382f
clintjeez <clintjeez@gmail.com>
2024-12-26 16:44:44 +0100

    animated title updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,10 @@ const About = () => {
         <h2 className='font-general text-sm uppercase md:text-[10px]'>
           Welcome to Zentry
         </h2>
-        <AnimatedTitle />
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventures"
+          containerClass='mt-5 !text-black text-center'
+        />
 
         <div className='about-subtext'>
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
