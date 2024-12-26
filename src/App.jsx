08504b8cfaa29eed87e4d8a958dfc89e00ee914c
clintjeez <clintjeez@gmail.com>
2024-12-26 16:12:56 +0100

    about section animation updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import Hero from './components/Hero';
+import About from './components/About';
 
 function App() {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero />
+      <About />
       <section className='z-0 min-h-screen bg-blue-500' />
     </main>
   );
