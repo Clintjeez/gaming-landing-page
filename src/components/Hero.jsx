e74ec204e04eee1bb692af84563ce4ccb523f939
clintjeez <clintjeez@gmail.com>
2024-12-26 13:53:05 +0100

    hero section animation updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,8 @@
 import { useRef, useState } from 'react';
+import Button from './Button';
+import { TiLocationArrow } from 'react-icons/ti';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -19,6 +23,32 @@ const Hero = () => {
     setHasClicked(true);
     setCurrentIndex(upcomingVideoIndex);
   };
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set('#next-video', { visibility: 'visible' });
+
+        gsap.to('#next-video', {
+          transformOrigin: 'center center',
+          scale: 1,
+          width: '100%',
+          height: '100%',
+          duration: 1,
+          ease: 'power1.inOut',
+          onStart: () => nextVideoRef.current.play(),
+        });
+
+        gsap.from('#current-video', {
+          transformOrigin: 'center center',
+          scale: 0,
+          duration: 1.5,
+          ease: 'power1.inOut',
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -57,7 +87,7 @@ const Hero = () => {
           />
 
           <video
-            autoPlay
+            // autoPlay
             loop
             muted
             src={getVideoSrc(
@@ -71,7 +101,26 @@ const Hero = () => {
         <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>
           G<b>a</b>ming
         </h1>
+        <div className='absolute left-0 top-0 z-40 size-full'>
+          <div className='mt-24 px-5 sm:px-10'>
+            <h1 className='special-font hero-heading text-blue-100'>
+              redefi<b>n</b>e
+            </h1>
+            <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
+              Enter the Metagame Layer <br /> Unleash the play Economy
+            </p>
+            <Button
+              id='watch-trailer'
+              title='Watch Trailer'
+              leftIcon={<TiLocationArrow />}
+              containerClass='!bg-yellow-300 flex-center-gap-1'
+            />
+          </div>
+        </div>
       </div>
+      <h1 className='special-font hero-heading absolute bottom-5 right-5 text-black'>
+        G<b>a</b>ming
+      </h1>
     </div>
   );
 };
